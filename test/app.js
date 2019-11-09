fb22ba4a17b7af84d0fdb05a654cf64aa3ae5549
Roman1104 <malakhov1104@gmail.com>
2019-11-09 21:41:26 +0500

    implement positive scenarios with UI and keyboard

--- test/app.js
+++ test/app.js
@@ -15,71 +15,142 @@ const expect = chai.expect;
   
   describe('Beru.ru authorization', function () {
     this.timeout(0);  //отключаем таймауты (2 секунды) для тестов
-    
     it('Check the page opens successfully.', async function () {
       await driver.get('https://beru.ru/');
       let title = await driver.getTitle();
       title.should.contain('Беру', 'The page title doesn\'t match the expected');
     });
 
-    it('Check the profile/login button name is "Войти в аккаунт"', async () => {
+    it('Check the profile/login button name is "Войти в аккаунт" for unauthentificated user', async () => {
+      await driver.get('https://beru.ru/');
       profileBtn = await driver.findElement(By.className('_3odNv2Dw2n'));
       let btnText = await profileBtn.getText();
       btnText.should.equal('Войти в аккаунт', 'Button title doesn\'t match:');
     });
 
-    it('Click the profile/login button', async () => {
-      try {
-        await profileBtn.click();
-        await driver.wait(until.urlContains('https://passport.yandex.ru/'), 10000);
-        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000);
-        await driver.wait(until.elementIsVisible(loginField), 10000);
-      } catch {
-        assert.fail('Failed to proceed user authorization');
-      }
+    it('Check the "Войти в аккаунт" button leads to authentification page', async () => {
+        await driver.get('https://beru.ru/');
+        await driver.findElement(By.className('_3odNv2Dw2n')).click();
+        let curUrl = await driver.getCurrentUrl();
+        try {
+          curUrl.should.contain('passport.yandex.ru','Failed to redirect to passport.yandex.ru');
+        }
+        catch(err) {
+          await driver.get('https://beru.ru/');
+          assert.fail(err);
+        }
+
+        //return to initial state
+        await driver.get('https://beru.ru/');
     });
 
-    it('Enter the login and press Enter', async () => {
-      try {
-        await loginField.sendKeys('testmalakhov2019@yandex.ru', Key.RETURN);
+    it('Authentificate user via UI elements', async () => {
+      await driver.get('https://beru.ru/');
+      await driver.findElement(By.className('_3odNv2Dw2n')).click();
       
-        passField = await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 10000);
-        await driver.wait(until.elementIsVisible(passField), 10000);
-      } catch {
-        assert.fail('Failed to proceed user authorization');
+      try {
+        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
+        await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
+        await loginField.sendKeys('testmalakhov2019@yandex.ru');
+        await driver.findElement(By.className('passp-sign-in-button')).click();
+      } catch(err) {
+        console.log(err);
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
       }
+
+      try {
+        passField = await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 10000, 'Failed to locate password field in the DOM');
+        await driver.wait(until.elementIsVisible(passField), 10000, 'Password field is not visible');
+        await passField.sendKeys('38tnrWW!QiNRqJv');
+        await driver.findElement(By.className('passp-form-button')).click();
+      } catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      try {
+        let curUrl = await driver.getCurrentUrl();
+        curUrl.should.contain('beru.ru','Failed to redirect back to the site');
+        profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000, 'Profile buttun not found');
+        let btnText = await profileBtn.getText();
+        btnText.should.equal('Мой профиль', 'Button title doesn\'t match:');
+        await profileBtn.click();
+        let profileTitle = await driver.wait(until.elementLocated(By.className('_2I5v9t-gmG')), 10000, 'Profile title not found');
+        await driver.wait(until.elementIsVisible(profileTitle), 10000, 'Profile title is not visible');
+        profileName = await profileTitle.getText();
+        profileName.should.equal('Automation Test Malakhov 2019', 'Profile name doesn\'t match:');
+      }
+      catch(err) {
+        await driver.get('https://beru.ru/logout?retpath=https%3A%2F%2Fberu.ru%2F%3Floggedin%3D1');
+        await driver.get('https://passport.yandex.ru/');
+        await driver.manage().deleteAllCookies();
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+      
+      //return to initial state
+      await driver.get('https://beru.ru/logout?retpath=https%3A%2F%2Fberu.ru%2F%3Floggedin%3D1');
+      await driver.get('https://passport.yandex.ru/');
+      await driver.manage().deleteAllCookies();
+      await driver.get('https://beru.ru/');
+
     });
 
-    it('Enter the password and press Enter', async () => {
+    it('Authentificate user via keyboard', async () => {
+      await driver.get('https://beru.ru/');
+      await driver.findElement(By.className('_3odNv2Dw2n')).click();
+      
       try {
-        await passField.sendKeys('38tnrWW!QiNRqJv', Key.RETURN);      
-        await driver.wait(until.urlContains('https://beru.ru/'), 10000);
-      } catch {
-        assert.fail('Failed to authorize user');
+        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
+        await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
+        await loginField.sendKeys('testmalakhov2019@yandex.ru', Key.RETURN);
+      } catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
       }
-    });
-    
-    it('Check the profile button name is "Мой профиль"', async () => {
-      profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000);
-      let btnText = await profileBtn.getText();
-      btnText.should.equal('Мой профиль', 'Button title doesn\'t match:');
-    });
-    
-    it('Click profile button and verify profile name', async () => {
+
       try {
+        passField = await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 10000, 'Failed to locate password field in the DOM');
+        await driver.wait(until.elementIsVisible(passField), 10000, 'Password field is not visible');
+        await passField.sendKeys('38tnrWW!QiNRqJv', Key.RETURN);
+      } catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      try {
+        let curUrl = await driver.getCurrentUrl();
+        curUrl.should.contain('beru.ru','Failed to redirect back to the site');
+        profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000, 'Profile buttun not found');
+        let btnText = await profileBtn.getText();
+        btnText.should.equal('Мой профиль', 'Button title doesn\'t match:');
         await profileBtn.click();
-        let profileTitle = await driver.wait(until.elementLocated(By.className('_2I5v9t-gmG')), 10000);
-        await driver.wait(until.elementIsVisible(profileTitle), 10000);
+        let profileTitle = await driver.wait(until.elementLocated(By.className('_2I5v9t-gmG')), 10000, 'Profile title not found');
+        await driver.wait(until.elementIsVisible(profileTitle), 10000, 'Profile title is not visible');
         profileName = await profileTitle.getText();
-      } catch {
-        assert.fail('Failed to open profile menu');
+        profileName.should.equal('Automation Test Malakhov 2019', 'Profile name doesn\'t match:');
       }
-      profileName.should.equal('Automation Test Malakhov 2019', 'Profile name doesn\'t match:');
+      catch(err) {
+        await driver.get('https://beru.ru/logout?retpath=https%3A%2F%2Fberu.ru%2F%3Floggedin%3D1');
+        await driver.get('https://passport.yandex.ru/');
+        await driver.manage().deleteAllCookies();
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+      
+      //return to initial state
+      await driver.get('https://beru.ru/logout?retpath=https%3A%2F%2Fberu.ru%2F%3Floggedin%3D1');
+      await driver.get('https://passport.yandex.ru/');
+      await driver.manage().deleteAllCookies();
+      await driver.get('https://beru.ru/');
+      
     });
     
     after(async function () {
       await driver.close();
     });
+    
   });
   run();
 })();
