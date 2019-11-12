a0e54aebfc724df11fbe7d4cacae4f09bf1c6e5c
roman1104 <malakhov1104@gmail.com>
2019-11-12 23:51:34 +0500

    add negative scenarios to authorization flow

--- test/app.js
+++ test/app.js
@@ -15,6 +15,7 @@ const expect = chai.expect;
   
   describe('Beru.ru authorization', function () {
     this.timeout(0);  //отключаем таймауты (2 секунды) для тестов
+    
     it('Check the page opens successfully.', async function () {
       await driver.get('https://beru.ru/');
       let title = await driver.getTitle();
@@ -45,8 +46,7 @@ const expect = chai.expect;
     });
 
     it('Authentificate user via UI elements', async () => {
-      await driver.get('https://beru.ru/');
-      await driver.findElement(By.className('_3odNv2Dw2n')).click();
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
         loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
@@ -98,8 +98,7 @@ const expect = chai.expect;
     });
 
     it('Authentificate user via keyboard', async () => {
-      await driver.get('https://beru.ru/');
-      await driver.findElement(By.className('_3odNv2Dw2n')).click();
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
         loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
@@ -146,7 +145,167 @@ const expect = chai.expect;
       await driver.get('https://beru.ru/');
       
     });
+
+    it('Check the error returns when unexisting login is submitted', async () => {
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
+      
+      try {
+        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
+        await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
+        await loginField.sendKeys('bkcglkhlcvb', Key.RETURN);
+        let errorBlock = await driver.wait(until.elementLocated(By.className('passp-form-field__error')), 2000, 'No error message in the DOM');
+        await driver.wait(until.elementIsVisible(errorBlock), 2000, 'Error message is not visible');
+        let errorText = await errorBlock.getText();
+        errorText.should.equal('Такого аккаунта нет', 'Incorrect error message text');
+      } catch(err) {
+        console.log(err);
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      //return to initial state
+      await driver.get('https://beru.ru/');
+
+    });
+
+    it('Check the error returns when incorrect login is submitted', async () => {
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
+      
+      try {
+        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
+        await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
+        await loginField.sendKeys('1111', Key.RETURN);
+        let errorBlock = await driver.wait(until.elementLocated(By.className('passp-form-field__error')), 2000, 'No error message in the DOM');
+        await driver.wait(until.elementIsVisible(errorBlock), 2000, 'Error message is not visible');
+        let errorText = await errorBlock.getText();
+        errorText.should.equal('Такой логин не подойдет', 'Incorrect error message text');
+      } catch(err) {
+        console.log(err);
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      //return to initial state
+      await driver.get('https://beru.ru/');
+
+    });
+
+    it('Check the error returns when no login is submitted', async () => {
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
+      
+      try {
+        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
+        await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
+        await loginField.sendKeys(Key.RETURN);
+        let errorBlock = await driver.wait(until.elementLocated(By.className('passp-form-field__error')), 2000, 'No error message in the DOM');
+        await driver.wait(until.elementIsVisible(errorBlock), 2000, 'Error message is not visible');
+        let errorText = await errorBlock.getText();
+        errorText.should.equal('Логин не указан', 'Incorrect error message text');
+      } catch(err) {
+        console.log(err);
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      //return to initial state
+      await driver.get('https://beru.ru/');
+
+    }); 
     
+    it('Check the back button on the login page returns to the site without authetification', async () => {
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
+      
+      await driver.findElement(By.className('passp-previous-step-button__icon')).click();
+      let curUrl = await driver.getCurrentUrl();
+      try {
+        curUrl.should.contain('beru.ru','Failed to redirect to passport.yandex.ru');
+        profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000, 'Profile button is missing on the page');
+        let btnText = await profileBtn.getText();
+        btnText.should.equal('Войти в аккаунт', 'Button title doesn\'t match:');
+      }
+      catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+    });
+
+
+    it('Check the error returns when incorreсt password is submitted', async () => {
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
+      
+      try {
+        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
+        await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
+        await loginField.sendKeys('testmalakhov2019@yandex.ru', Key.RETURN);
+
+        passField = await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 10000, 'Failed to locate password field in the DOM');
+        await driver.wait(until.elementIsVisible(passField), 10000, 'Password field is not visible');
+        await passField.sendKeys('38tnrWW!qiNRqJv', Key.RETURN);
+      } catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      try {
+        let curUrl = await driver.getCurrentUrl();
+        curUrl.should.contain('passport.yandex.ru','Unexpected redirect occured');
+        let errorBlock = await driver.wait(until.elementLocated(By.className('passp-form-field__error')), 2000, 'No error message in the DOM');
+        await driver.wait(until.elementIsVisible(errorBlock), 2000, 'Error message is not visible');
+        let errorText = await errorBlock.getText();
+        errorText.should.equal('Неверный пароль', 'Incorrect error message text');
+      }
+      catch(err) {
+        await driver.get('https://beru.ru/logout?retpath=https%3A%2F%2Fberu.ru%2F%3Floggedin%3D1');
+        await driver.get('https://passport.yandex.ru/');
+        await driver.manage().deleteAllCookies();
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+      
+      //return to initial state
+      await driver.manage().deleteAllCookies();
+      await driver.get('https://beru.ru/');
+      
+    });
+    
+    it('Check the error returns when no password is submitted', async () => {
+      await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
+      
+      try {
+        loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
+        await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
+        await loginField.sendKeys('testmalakhov2019@yandex.ru', Key.RETURN);
+
+        passField = await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 10000, 'Failed to locate password field in the DOM');
+        await driver.wait(until.elementIsVisible(passField), 10000, 'Password field is not visible');
+        await passField.sendKeys(Key.RETURN);
+      } catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      try {
+        let curUrl = await driver.getCurrentUrl();
+        curUrl.should.contain('passport.yandex.ru','Unexpected redirect occured');
+        let errorBlock = await driver.wait(until.elementLocated(By.className('passp-form-field__error')), 2000, 'No error message in the DOM');
+        await driver.wait(until.elementIsVisible(errorBlock), 2000, 'Error message is not visible');
+        let errorText = await errorBlock.getText();
+        errorText.should.equal('Пароль не указан', 'Incorrect error message text');
+      }
+      catch(err) {
+        await driver.get('https://beru.ru/logout?retpath=https%3A%2F%2Fberu.ru%2F%3Floggedin%3D1');
+        await driver.get('https://passport.yandex.ru/');
+        await driver.manage().deleteAllCookies();
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+      
+      //return to initial state
+      await driver.manage().deleteAllCookies();
+      await driver.get('https://beru.ru/');
+      
+    });
+
     after(async function () {
       await driver.close();
     });
