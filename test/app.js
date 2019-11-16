545b9b4aa89963fb78385a5e4188237ecc941702
roman1104 <malakhov1104@gmail.com>
2019-11-17 01:02:19 +0500

    add opening of city selection popup

--- test/app.js
+++ test/app.js
@@ -8,13 +8,14 @@ const expect = chai.expect;
 (async function() {
   let driver = await new Builder().forBrowser('chrome').build();
   await driver.manage().window().maximize();
-  let profileBtn;
-  let loginField;
-  let passField;
-  let profileName;
+  
   
   describe('Beru.ru authorization', function () {
     this.timeout(0);  //отключаем таймауты (2 секунды) для тестов
+    let profileBtn;
+    let loginField;
+    let passField;
+    let profileName;
     
     it('Check the page opens successfully.', async function () {
       await driver.get('https://beru.ru/');
@@ -75,6 +76,7 @@ const expect = chai.expect;
         profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000, 'Profile buttun not found');
         let btnText = await profileBtn.getText();
         btnText.should.equal('Мой профиль', 'Button title doesn\'t match:');
+        await driver.wait(until.elementIsVisible(profileBtn), 10000, 'Profile button is invisible');
         await profileBtn.click();
         let profileTitle = await driver.wait(until.elementLocated(By.className('_2I5v9t-gmG')), 10000, 'Profile title not found');
         await driver.wait(until.elementIsVisible(profileTitle), 10000, 'Profile title is not visible');
@@ -99,7 +101,7 @@ const expect = chai.expect;
 
     it('Authentificate user via keyboard', async () => {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
-      
+
       try {
         loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
         await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
@@ -121,7 +123,8 @@ const expect = chai.expect;
       try {
         let curUrl = await driver.getCurrentUrl();
         curUrl.should.contain('beru.ru','Failed to redirect back to the site');
-        profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000, 'Profile buttun not found');
+        profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000, 'Profile button not found');
+        await driver.wait(until.elementIsVisible(profileBtn), 10000, 'Profile button is invisible');
         let btnText = await profileBtn.getText();
         btnText.should.equal('Мой профиль', 'Button title doesn\'t match:');
         await profileBtn.click();
@@ -145,7 +148,7 @@ const expect = chai.expect;
       await driver.get('https://beru.ru/');
       
     });
-
+    
     it('Check the error returns when unexisting login is submitted', async () => {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
@@ -305,11 +308,60 @@ const expect = chai.expect;
       await driver.get('https://beru.ru/');
       
     });
-
-    after(async function () {
-      await driver.close();
-    });
     
+    after(async function () {
+      //await driver.close();
+    });
   });
+
+  describe('City selection flow', function () {
+    this.timeout(0);  //отключаем таймауты (2 секунды) для тестов
+
+    it('Check the city input field appears', async function () {
+      await driver.get('https://beru.ru/');
+      
+      try {
+        let city = await driver.findElement(By.xpath('//div[@class="EsYwYP7LNa"]/span[@class="-soJAyMJBd"]/span[@class="_2XJ6yiRp5w"]'));
+        await city.click();      
+        
+        let inputFields = await driver.findElements(By.xpath('//div[@class="_1U2ErCeoqP"]//input[@class="_2JDvXzYsUI"]'));
+        should.equal(inputFields.length, 1, 'Zero or more than one city input field is found');
+        let cityInput = inputFields[0];
+        let isVisible = await cityInput.isDisplayed();
+        let isEditable =  await cityInput.isEnabled();
+        should.equal(isVisible, true, 'The city input field is invisible');
+        should.equal(isEditable, true, 'The city input field can\'t be edited');
+      } catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+      //return to initial state
+      await driver.get('https://beru.ru/');      
+    });
+
+    it('Check the quick city suggestions are displayed on typing', async function () {
+      await driver.get('https://beru.ru/');
+      try {
+        let city = await driver.findElement(By.xpath('//div[@class="EsYwYP7LNa"]/span[@class="-soJAyMJBd"]/span[@class="_2XJ6yiRp5w"]'));
+        await city.click();              
+        let inputField = await driver.findElement(By.xpath('//div[@class="_1U2ErCeoqP"]//input[@class="_2JDvXzYsUI"]'));
+        inputField.sendKeys('хвал');
+        let suggestsList = await driver.wait(until.elementLocated(By.id('react-autowhatever-region')), 30000, 'Quick suggests list is not shown');
+        await driver.wait(until.elementIsVisible(suggestsList), 2000, 'Cities suggests list is not visible');
+
+      } catch(err) {
+        await driver.get('https://beru.ru/');
+        assert.fail(err);
+      }
+
+    //return to initial state
+    await driver.get('https://beru.ru/');  
+
+    });
+
+
+  });
+
   run();
 })();
