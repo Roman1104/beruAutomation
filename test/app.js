5f2c75c1bc6192d40e2d14e2e8f0215b77b76cfb
Roman1104 <malakhov1104@gmail.com>
2019-11-18 10:58:25 +0500

    refactor authentification flow tests

--- test/app.js
+++ test/app.js
@@ -5,31 +5,37 @@ const assert = chai.assert;
 const should = chai.should();
 const expect = chai.expect;
 
-(async function() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  await driver.manage().window().maximize();
-  let profileBtn;
-  let loginField;
-  let passField;
-  let profileName;
-  
-  describe('Beru.ru authorization', function () {
+describe('Beru.ru authorization flow', function () {
     this.timeout(0);  //отключаем таймауты (2 секунды) для тестов
-    
+    let driver; 
+    let profileBtn;
+    let loginField;
+    let passField;
+    let profileName;
+
+    before(async function () {
+      driver = await new Builder().forBrowser('chrome').build();
+      await driver.manage().window().maximize();
+    });
+
+    after(async function () {
+      await driver.close();
+    });
+
     it('Check the page opens successfully.', async function () {
       await driver.get('https://beru.ru/');
       let title = await driver.getTitle();
       title.should.contain('Беру', 'The page title doesn\'t match the expected');
     });
 
-    it('Check the profile/login button name is "Войти в аккаунт" for unauthentificated user', async () => {
+    it('Check the profile/login button name is "Войти в аккаунт" for unauthentificated user', async function () {
       await driver.get('https://beru.ru/');
       profileBtn = await driver.findElement(By.className('_3odNv2Dw2n'));
       let btnText = await profileBtn.getText();
       btnText.should.equal('Войти в аккаунт', 'Button title doesn\'t match:');
     });
 
-    it('Check the "Войти в аккаунт" button leads to authentification page', async () => {
+    it('Check the "Войти в аккаунт" button leads to authentification page', async function () {
         await driver.get('https://beru.ru/');
         await driver.findElement(By.className('_3odNv2Dw2n')).click();
         let curUrl = await driver.getCurrentUrl();
@@ -45,7 +51,7 @@ const expect = chai.expect;
         await driver.get('https://beru.ru/');
     });
 
-    it('Authentificate user via UI elements', async () => {
+    it('Authentificate user via UI elements', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
@@ -53,13 +59,7 @@ const expect = chai.expect;
         await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
         await loginField.sendKeys('testmalakhov2019@yandex.ru');
         await driver.findElement(By.className('passp-sign-in-button')).click();
-      } catch(err) {
-        console.log(err);
-        await driver.get('https://beru.ru/');
-        assert.fail(err);
-      }
-
-      try {
+      
         passField = await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 10000, 'Failed to locate password field in the DOM');
         await driver.wait(until.elementIsVisible(passField), 10000, 'Password field is not visible');
         await passField.sendKeys('38tnrWW!QiNRqJv');
@@ -97,19 +97,14 @@ const expect = chai.expect;
 
     });
 
-    it('Authentificate user via keyboard', async () => {
+    it('Authentificate user via keyboard', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
         loginField = await driver.wait(until.elementLocated(By.id('passp-field-login')), 10000, 'Failed to locate login field in the DOM');
         await driver.wait(until.elementIsVisible(loginField), 10000, 'Login field is not visible');
         await loginField.sendKeys('testmalakhov2019@yandex.ru', Key.RETURN);
-      } catch(err) {
-        await driver.get('https://beru.ru/');
-        assert.fail(err);
-      }
-
-      try {
+      
         passField = await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 10000, 'Failed to locate password field in the DOM');
         await driver.wait(until.elementIsVisible(passField), 10000, 'Password field is not visible');
         await passField.sendKeys('38tnrWW!QiNRqJv', Key.RETURN);
@@ -146,7 +141,7 @@ const expect = chai.expect;
       
     });
 
-    it('Check the error returns when unexisting login is submitted', async () => {
+    it('Check the error returns when unexisting login is submitted', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
@@ -158,7 +153,6 @@ const expect = chai.expect;
         let errorText = await errorBlock.getText();
         errorText.should.equal('Такого аккаунта нет', 'Incorrect error message text');
       } catch(err) {
-        console.log(err);
         await driver.get('https://beru.ru/');
         assert.fail(err);
       }
@@ -168,7 +162,7 @@ const expect = chai.expect;
 
     });
 
-    it('Check the error returns when incorrect login is submitted', async () => {
+    it('Check the error returns when incorrect login is submitted', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
@@ -180,7 +174,6 @@ const expect = chai.expect;
         let errorText = await errorBlock.getText();
         errorText.should.equal('Такой логин не подойдет', 'Incorrect error message text');
       } catch(err) {
-        console.log(err);
         await driver.get('https://beru.ru/');
         assert.fail(err);
       }
@@ -190,7 +183,7 @@ const expect = chai.expect;
 
     });
 
-    it('Check the error returns when no login is submitted', async () => {
+    it('Check the error returns when no login is submitted', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
@@ -202,7 +195,6 @@ const expect = chai.expect;
         let errorText = await errorBlock.getText();
         errorText.should.equal('Логин не указан', 'Incorrect error message text');
       } catch(err) {
-        console.log(err);
         await driver.get('https://beru.ru/');
         assert.fail(err);
       }
@@ -212,12 +204,12 @@ const expect = chai.expect;
 
     }); 
     
-    it('Check the back button on the login page returns to the site without authetification', async () => {
+    it('Check the back button on the login page returns to the site without authetification', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
-      await driver.findElement(By.className('passp-previous-step-button__icon')).click();
-      let curUrl = await driver.getCurrentUrl();
       try {
+        await driver.findElement(By.className('passp-previous-step-button__icon')).click();
+        let curUrl = await driver.getCurrentUrl();
         curUrl.should.contain('beru.ru','Failed to redirect to passport.yandex.ru');
         profileBtn = await driver.wait(until.elementLocated(By.className('_3odNv2Dw2n')), 10000, 'Profile button is missing on the page');
         let btnText = await profileBtn.getText();
@@ -230,7 +222,7 @@ const expect = chai.expect;
     });
 
 
-    it('Check the error returns when incorreсt password is submitted', async () => {
+    it('Check the error returns when incorreсt password is submitted', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
@@ -268,7 +260,7 @@ const expect = chai.expect;
       
     });
     
-    it('Check the error returns when no password is submitted', async () => {
+    it('Check the error returns when no password is submitted', async function () {
       await driver.get('https://beru.ru/login?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D8255');
       
       try {
@@ -305,11 +297,4 @@ const expect = chai.expect;
       await driver.get('https://beru.ru/');
       
     });
-
-    after(async function () {
-      await driver.close();
-    });
-    
-  });
-  run();
-})();
+});
